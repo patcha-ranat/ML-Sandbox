a2ea974ef926ad43aab8f5019a8fa315ed7d29d1
ken-patcharanat <patcha-ranat@outlook.com>
2025-02-25 20:00:29 +0700

    agentic workflow dpe poc crewai state

--- agentic_workflow/dpe_optimization/src/dpe_optimization/crews/analyst_crew/analyst_crew.py
+++ agentic_workflow/dpe_optimization/src/dpe_optimization/crews/analyst_crew/analyst_crew.py
@@ -1,18 +1,27 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
+from crewai.knowledge.source.csv_knowledge_source import CSVKnowledgeSource
 
 
 @CrewBase
-class AnalystCrew:
+class AnalystCrew():
     """Analyst Crew"""
 
-    agents_config = "config/agents.yaml"
-    tasks_config = "config/tasks.yaml"
+    def __init__(self, custom_inputs: dict):
+        super().__init__()
+
+        self.custom_inputs = custom_inputs
+        self.agents_config = "config/agents.yaml"
+        self.tasks_config = "config/tasks.yaml"
+        self.csv_source = CSVKnowledgeSource(
+            file_paths=[f"data_{self.custom_inputs.get('exec_date')}.csv"]
+        )
 
     @agent
     def analyze_agent(self) -> Agent:
         return Agent(
             config=self.agents_config["analyze_agent"],
+            llm="gpt-4o-mini"
         )
     
     @task
@@ -29,5 +38,6 @@ class AnalystCrew:
             agents=self.agents,
             tasks=self.tasks,
             process=Process.sequential,
+            knowledge_sources=[self.csv_source],
             verbose=True,
         )
--- agentic_workflow/dpe_optimization/src/dpe_optimization/main.py
+++ agentic_workflow/dpe_optimization/src/dpe_optimization/main.py
@@ -1,57 +1,135 @@
-import logging
-from pydantic import BaseModel
 
-from crewai.flow import Flow, listen, start, router, or_
+import os
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+import json
+
+from crewai.flow import Flow, listen, start, router, or_, persist
 
 from dpe_optimization.crews.analyst_crew.analyst_crew import AnalystCrew
 from dpe_optimization.crews.solution_crew.solution_crew import SolutionCrew
 from dpe_optimization.crews.checker_crew.checker_crew import CheckerCrew
 
 
-# set logger
-import logging
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
+# Set Logger
+# logging.basicConfig()
+# logging.getLogger().setLevel(logging.INFO)
+
+# Global Variables
+EXEC_DATE = datetime.today().strftime("%Y-%m-%d")
+EXEC_UTC_DATE = datetime.utcnow().date().strftime("%Y-%m-%d")
+FLOW_TIMER = datetime.now()
 
 
 class ResourceOptimizerState(BaseModel):
-    sentence_count: int = 1
-    poem: str = ""
+    # Note: 'id' field is automatically added to all states
+    # Structure State not supported dict type annotations: TypeError: Expected two type arguments for <class 'dict'>, got 1
+    
+    # General Metadata
+    metadata: dict = {
+        "execution_date": EXEC_DATE,
+        "execution_date_utc": EXEC_UTC_DATE,
+        "execution_time": None
+    }
+    
+    # Crew Input
+    flow_input: dict = {
+        "analyst": "Analyze financial data and conclude expense based on categories"
+    }
+    
+    # Flow Output
+    flow_output: dict = {
+        "extract_read_task": {
+            "execution_time": None,
+            "output": None 
+        },
+        "analyze_task": {
+            "execution_time": None,
+            "output": None,
+            "token_usage": None
+        },
+        # other flow tasks
+    }
+
+    def to_dict(self):
+        return {
+            "metadata": self.metadata,
+            "flow_input": self.flow_input,
+            "flow_output": self.flow_output,
+        }
+
+    def convert_execution_time(self):
+        """Convert by applying -> lambda element: str(timedelta(seconds=<element>.seconds))"""
+        self.metadata["execution_time"] = str(timedelta(seconds=self.metadata["execution_time"].seconds))
+        self.flow_output["extract_read_task"]["execution_time"] = str(timedelta(seconds=self.flow_output["extract_read_task"]["execution_time"].seconds))
+        self.flow_output["analyze_task"]["execution_time"] = str(timedelta(seconds=self.flow_output["analyze_task"]["execution_time"].seconds))
+        return self
 
 
 class ResourceOptimizerFlow(Flow[ResourceOptimizerState]):
 
     @start()
-    def extract_read(self):
-        pass
+    def extract_read_task(self):
+        """Extract Input to knowledge directories"""
+        start_time = datetime.now()
+        
+        file_name = f"data_{EXEC_DATE}"
+        if os.path.isfile(path=f"knowledge/{file_name}"):
+            print(f"{file_name} does exist, skipping extract")
+        else:
+            print(f"{file_name} does not exist, starting knowledge input extraction")
+            # extract metrics to files by sql
+            pass
+        
+        # log output
+        end_time = datetime.now()
+        self.state.flow_output["extract_read_task"]["execution_time"] = end_time - start_time
 
-    @router(extract_read)
+    @router(extract_read_task)
     def analyze(self):
-        # need a crew
-        result = AnalystCrew()
-        if result is True:
-            return "detect"
-        else:
-            return "not detect"
+        start_time = datetime.now()
 
-    @listen(or_("detect", "failed"))
-    def provide_solution():
         # need a crew
-        result = SolutionCrew()
-        pass
+        inputs = {
+            "exec_date": EXEC_DATE,
+            "question": self.state.flow_input.get("analyst")
+        }
+        result = AnalystCrew(custom_inputs=inputs).crew().kickoff(inputs=inputs)
+        end_time = datetime.now()
+        
+        # log output
+        self.state.flow_output["analyze_task"]["execution_time"] = end_time - start_time
+        self.state.flow_output["analyze_task"]["output"] = result.raw
+        self.state.flow_output["analyze_task"]["token_usage"] = result.token_usage.__dict__
+        
+        # if result is True:
+        #     return "detect"
+        # else:
+        #     return "not detect"
 
-    @router(provide_solution)
-    def check_result(self):
-        # need a crew
-        result = CheckerCrew()
-        if result is True:
-            return "success"
-        else:
-            return "failed"
+    # @listen(or_("detect", "failed"))
+    # def provide_solution():
+    #     # need a crew
+    #     result = SolutionCrew()
+    #     pass
 
-    @listen(or_("success", "not detect"))
+    # @router(provide_solution)
+    # def check_result(self):
+    #     # need a crew
+    #     result = CheckerCrew()
+    #     if result is True:
+    #         return "success"
+    #     else:
+    #         return "failed"
+
+    # @listen(or_("success", "not detect"))
+    @listen(analyze)
     def write_output_send_noti(self):
-        pass
+        self.state.metadata["execution_time"] = datetime.now() - FLOW_TIMER
+        with open("state_output.json", "w") as f:
+            json.dump(self.state.convert_execution_time().to_dict(), f, indent=4)
+            f.close()
+        print("Writing State File Output Success")
 
     # @listen(write_output_send_noti)
     # def implement(self):
